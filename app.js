ff0a062487f0e82c806d933975c4b3d085c55163
Yimo <heh1@live.fr>
2018-08-05 11:59:07 +0100

    Some tutorials later...

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 const http = require('http');
+const fs = require('fs');
 
+var myReadStream = fs.createReadStream(__dirname+'/readme.txt', {'encoding':'utf8', 'highWaterMark':64});
+var myWriteStream = fs.createWriteStream(__dirname+'/writeme.txt', {'encoding':'utf8'});
+
+/* // smart way of doing this
+yReadStream.on('data', function(chunk){
+  console.log('new chunk received');
+  myWriteStream.write(chunk);
+});
+*/
+
+myReadStream.pipe(myWriteStream);
+
+/*
 var server = http.createServer(
   function(req, res) {
     console.log(`request was made: `+req.url)
@@ -9,3 +23,4 @@ var server = http.createServer(
 
 server.listen(3000, '127.0.0.1');
 console.log('Listening to part 3000');
+*/
