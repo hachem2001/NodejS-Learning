e75f0fcbfe5bf8da9aa8425576a64a96b5433cf0
Yimo <heh1@live.fr>
2018-08-05 17:49:07 +0100

    Simple JSON, next is ep 19

--- app.js
+++ app.js
@@ -14,9 +14,13 @@ yReadStream.on('data', function(chunk){
 var server = http.createServer(
   function(req, res) {
     console.log(`request was made: `+req.url)
-    res.writeHead(200, {'Content-type':'text/html'});
-    let myReadStream = fs.createReadStream(__dirname+'/index.html', {'encoding':'utf8'});
-    myReadStream.pipe(res);
+    res.writeHead(200, {'Content-type':'application/json'});
+    var myObj = {
+      name: 'Ryu',
+      job: 'Ninja',
+      age : 29
+    }
+    res.end(JSON.stringify(myObj))
   });
 
 server.listen(3000, '127.0.0.1');
