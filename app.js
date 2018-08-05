1c84a747d229280a3b6856258e618e751571cc19
Yimo <heh1@live.fr>
2018-08-05 17:20:23 +0100

    Just some other commit. next is episode 18

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const http = require('http');
 const fs = require('fs');
 
-var myReadStream = fs.createReadStream(__dirname+'/readme.txt', {'encoding':'utf8', 'highWaterMark':64});
-var myWriteStream = fs.createWriteStream(__dirname+'/writeme.txt', {'encoding':'utf8'});
 
 /* // smart way of doing this
 yReadStream.on('data', function(chunk){
@@ -11,16 +9,15 @@ yReadStream.on('data', function(chunk){
 });
 */
 
-myReadStream.pipe(myWriteStream);
 
-/*
+
 var server = http.createServer(
   function(req, res) {
     console.log(`request was made: `+req.url)
-    res.writeHead(200, {'Content-type':'text/plain'});
-    res.end('Hey humans');
+    res.writeHead(200, {'Content-type':'text/html'});
+    let myReadStream = fs.createReadStream(__dirname+'/index.html', {'encoding':'utf8'});
+    myReadStream.pipe(res);
   });
 
 server.listen(3000, '127.0.0.1');
 console.log('Listening to part 3000');
-*/
